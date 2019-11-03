46b450c1f4f00cc797ed3974b4ac26e2fe2445cd
Michael Crumm <mike@crumm.net>
2019-11-03 13:14:39 -0800

    add live pagination + page summary

--- lib/live_table_web/views/company_view.ex
+++ lib/live_table_web/views/company_view.ex
@@ -1,4 +1,19 @@
 defmodule LiveTableWeb.CompanyView do
   use LiveTableWeb, :view
   use Scrivener.HTML
+  alias Scrivener.Page
+
+  def page_summary(%Page{} = page) do
+    %{page_number: num, page_size: size, total_entries: total} = page
+    page_summary(num, size, total)
+  end
+
+  defp page_summary(1, size, total) do
+    "Showing 1 to #{size} of #{total} entries"
+  end
+
+  defp page_summary(page, size, total) do
+    x = size * (page - 1)
+    "Showing #{x + 1} to #{x + size} of #{total} entries"
+  end
 end
