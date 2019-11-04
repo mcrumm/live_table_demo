8a3a01447128f43c82bc65cb45ca9916f031109e
Michael Crumm <mike@crumm.net>
2019-11-04 12:31:26 -0800

    add some filters

--- lib/live_table/orders/company_query.ex
+++ lib/live_table/orders/company_query.ex
@@ -2,5 +2,18 @@ defmodule LiveTable.Orders.CompanyQuery do
   require Ecto.Query
 
   use Inquisitor
+  use Inquisitor.JsonApi.Filter
   use Inquisitor.JsonApi.Sort
+
+  def build_filter_query(query, "name", value, _) do
+    Ecto.Query.where(query, [c], ilike(c.name, ^"%#{value}%"))
+  end
+
+  def build_filter_query(query, "city", value, _) do
+    Ecto.Query.where(query, [c], ilike(c.city, ^"#{value}%"))
+  end
+
+  def build_filter_query(query, "state", value, _) do
+    Ecto.Query.where(query, [c], c.state == ^value)
+  end
 end
--- lib/live_table_web/live/companies_live.ex
+++ lib/live_table_web/live/companies_live.ex
@@ -3,12 +3,14 @@ defmodule LiveTableWeb.CompaniesLive do
 
   alias LiveTable.Orders
   alias LiveTableWeb.CompanyView
+  alias LiveTableWeb.Router.Helpers, as: Routes
 
   @defaults %{"sort" => "id"}
+  @valid_filters ~w(name city state)
 
   @impl true
   def mount(_session, socket) do
-    {:ok, assign(socket, page: %Scrivener.Page{})}
+    {:ok, assign(socket, page: %Scrivener.Page{page_number: 1}, params: %{}, raw_params: %{})}
   end
 
   @impl true
@@ -20,7 +22,24 @@ defmodule LiveTableWeb.CompaniesLive do
 
     link_params = link_params(params)
 
-    {:noreply, assign(socket, page: page, params: link_params)}
+    {:noreply, assign(socket, page: page, params: link_params, raw_params: params)}
+  end
+
+  @impl true
+  def handle_event(<<"filter-", key::binary>>, %{"value" => value}, socket)
+      when key in @valid_filters do
+    default_filter = if value == "", do: %{}, else: Map.put(%{}, key, value)
+    # update the current params from the socket
+    raw_params =
+      Map.update(socket.assigns.raw_params, "filter", default_filter, fn
+        filter when is_map(filter) -> Map.put(filter, key, value)
+      end)
+
+    link_params =
+      [page: socket.assigns.page.page_number] ++
+        link_params(raw_params)
+
+    {:noreply, live_redirect(socket, to: Routes.company_path(socket, __MODULE__, link_params))}
   end
 
   @impl true
@@ -30,7 +49,7 @@ defmodule LiveTableWeb.CompaniesLive do
 
   defp link_params(params) do
     params
-    |> Map.take(~w(sort))
+    |> Map.take(~w(sort filter))
     |> Enum.into([], fn {k, v} -> {String.to_atom(k), v} end)
   end
 end
